2062b8ea30f734d03234a951da4636566945b95c
mgnoonan <noonanconsulting@outlook.com>
2018-11-24 11:28:19 -0500

    Add search page

---
+++ RssFeeder.Mvc/Models/SiteParserSearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RssFeeder.Mvc.Models
+{
+    public class SiteParserSearchModel
+    {
+        public string SiteName { get; set; }
+        public Guid SiteGuid { get; set; }
+    }
+}
