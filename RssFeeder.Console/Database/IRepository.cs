979f4082eb089dc44b5db268c2b5840edb9734c9
Matthew Noonan <mnoonan@msn.com>
2020-07-04 21:53:51 -0400

    refactored database code to IRepository pattern

---
+++ RssFeeder.Console/Database/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RssFeeder.Console.Database
+{
+    public interface IRepository
+    {
+        List<T> GetDocuments<T>(string collectionName, Func<T, bool> predicate);
+        void CreateDocument<T>(string collectionName, T item);
+        bool DocumentExists<T>(string collectionName, Func<T, bool> predicate);
+        void DeleteDocument<T>(string collectionName, string documentID, string partitionKey);
+    }
+}
