8d99cc47d90a276c2e7dd99582bab28f2bae85e2
Matthew Noonan <mnoonan@msn.com>
2020-07-23 18:14:27 -0400

    Fixed article crawling

---
+++ RssFeeder.Console/ArticleDefinitions/IArticleDefinitionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RssFeeder.Models;
+
+namespace RssFeeder.Console.ArticleDefinitions
+{
+    public interface IArticleDefinitionFactory
+    {
+        SiteArticleDefinition Get(string sitename);
+    }
+}
