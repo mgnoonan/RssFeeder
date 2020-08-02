fc7443da427f3a557aa1738ee08a17e2621e0607
Matthew Noonan <mnoonan@msn.com>
2020-08-02 17:26:34 -0400

    upgraded the crawler downloads to use SocketsHttpHandler

--- RssFeeder.Console/Utility/WebUtils.cs
+++ RssFeeder.Console/Utility/WebUtils.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Security;
 using System.Reflection;
 using System.Text;
 using HtmlAgilityPack;
@@ -18,45 +19,37 @@ namespace RssFeeder.Console.Utility
     /// </summary>
     public class WebUtils : IWebUtils
     {
-        public WebUtils() { }
+        private const string USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4170.0 Safari/537.36 Edg/85.0.552.1";
+        private static HttpClient _client;
+
+        public WebUtils()
+        {
+            if (_client == null)
+            {
+                _client = new HttpClient(new SocketsHttpHandler
+                {
+                    AllowAutoRedirect = true,
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                    SslOptions = new SslClientAuthenticationOptions
+                    {
+                        AllowRenegotiation = true,
+                        EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12
+                    },
+                    UseCookies = true
+                });
+
+                _client.DefaultRequestHeaders.Add("user-agent", USERAGENT);
+            }
+        }
 
         public string DownloadString(string url)
         {
-            using WebClient client = new WebClient();
-            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4170.0 Safari/537.36 Edg/85.0.552.1");
-            return client.DownloadString(url);
+            return DownloadStringWithCompression(url);
         }
 
         public string DownloadStringWithCompression(string url)
         {
-            // Create web request that allows for compressed content
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            req.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate";
-
-            string source = String.Empty;
-
-            using (WebResponse webResponse = req.GetResponse())
-            {
-                using HttpWebResponse httpWebResponse = webResponse as HttpWebResponse;
-                StreamReader reader;
-                if (httpWebResponse.ContentEncoding?.ToLower().Contains("gzip") ?? false)
-                {
-                    reader = new StreamReader(new GZipStream(httpWebResponse.GetResponseStream(), CompressionMode.Decompress));
-                }
-                else if (httpWebResponse.ContentEncoding?.ToLower().Contains("deflate") ?? false)
-                {
-                    reader = new StreamReader(new DeflateStream(httpWebResponse.GetResponseStream(), CompressionMode.Decompress));
-                }
-                else
-                {
-                    reader = new StreamReader(httpWebResponse.GetResponseStream());
-                }
-                source = reader.ReadToEnd();
-            }
-
-            req.Abort();
-
-            return source;
+            return _client.GetStringAsync(url).Result;
         }
 
         public string SaveUrlToDisk(string url, string urlHash, string filename, bool removeScriptElements = true)
