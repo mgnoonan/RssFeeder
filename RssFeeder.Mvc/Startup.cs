07ec1f2f0dc023709d49dabc2dcfc7fe4449e785
mgnoonan <mnoonan@msn.com>
2020-11-08 20:42:31 -0500

    Update Startup.cs

--- RssFeeder.Mvc/Startup.cs
+++ RssFeeder.Mvc/Startup.cs
@@ -66,6 +66,7 @@ namespace RssFeeder.Mvc
             }
 
             var options = new RewriteOptions()
+                    .AddRedirect(@"(.*)\/\.env(.*)", "/")
                     .AddRedirect(@"(.*)\/wp-admin(.*)", "/")
                     .AddRedirect(@"(.*)\/wp-content(.*)", "/")
                     .AddRedirect(@"(.*)\/wp-includes(.*)", "/")
