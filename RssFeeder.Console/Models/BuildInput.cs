c5670e6fddb40416bfed5a2c6811ab0d98d5044e
Matthew Noonan <mnoonan@msn.com>
2021-05-01 22:44:05 -0400

    conversion to Oakton style command and input

---
+++ RssFeeder.Console/Models/BuildInput.cs
@@ -0,0 +1,10 @@
+﻿using Oakton;
+
+namespace RssFeeder.Console.Models
+{
+    public class BuildInput
+    {
+        [Description("The configuration file to build the RSS feeds")]
+        public string ConfigFile { get; set; }
+    }
+}
