f03d329481d364eb11add7c7ca0989bae42f2d7f
Matthew Noonan <mnoonan@msn.com>
2021-05-10 23:00:56 -0400

    added populist press feed

---
+++ RssFeeder.Console/Commands/ParseCommand.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using Autofac;
+using HtmlAgilityPack;
+using Oakton;
+using RssFeeder.Console.Models;
+using RssFeeder.Console.Parsers;
+using RssFeeder.Console.Utility;
+using Serilog;
+
+namespace RssFeeder.Console.Commands
+{
+    [Description("Parse article data from the specified URL")]
+    public class ParseCommand : OaktonCommand<ParseInput>
+    {
+        private readonly IContainer _container;
+
+        public ParseCommand(IContainer container)
+        {
+            _container = container;
+
+            // The usage pattern definition here is completely
+            // optional
+            Usage("Parse URL using default parser").Arguments(x => x.Url);
+            Usage("Parse URL using specified parser").Arguments(x => x.Url, x => x.Parser);
+            Usage("Parse URL using specified parser and selectors").Arguments(x => x.Url, x => x.Parser, x => x.BodySelector, x => x.ParagraphSelector);
+        }
+
+        public override bool Execute(ParseInput input)
+        {
+            var utils = _container.Resolve<IUtils>();
+            var webUtils = _container.Resolve<IWebUtils>();
+            var parser = _container.ResolveNamed<IArticleParser>(input.Parser);
+
+            string urlHash = utils.CreateMD5Hash(input.Url);
+            string html = webUtils.DownloadStringWithCompression(input.Url);
+
+            var doc = new HtmlDocument();
+            doc.Load(new StringReader(html));
+
+            Log.Information("MD5 Hash = '{UrlHash}'", urlHash);
+            Log.Information("og:site_name = '{SiteName}'", ParseMetaTagAttributes(doc, "og:site_name", "content").ToLower());
+            Log.Information("og:title = '{Title}'", ParseMetaTagAttributes(doc, "og:title", "content"));
+            Log.Information("og:description = '{Description}'", ParseMetaTagAttributes(doc, "og:description", "content"));
+            Log.Information("og:image = '{Image}'", ParseMetaTagAttributes(doc, "og:image", "content"));
+
+            string articleText = parser.GetArticleBySelector(doc.Text, input.BodySelector, input.ParagraphSelector);
+            Log.Information("Article text = '{ArticleText}'", articleText);
+            Log.CloseAndFlush();
+
+            // Just telling the OS that the command
+            // finished up okay
+            return true;
+        }
+
+        private string ParseMetaTagAttributes(HtmlDocument doc, string property, string attribute)
+        {
+            // Retrieve the requested meta tag by property name
+            var node = doc.DocumentNode.SelectSingleNode($"//meta[@property='{property}']");
+
+            // Node can come back null if the meta tag is not present in the DOM
+            // Attribute can come back null as well if not present on the meta tag
+            string value = node?.Attributes[attribute]?.Value.Trim() ?? string.Empty;
+
+            return value;
+        }
+    }
+}
---
+++ RssFeeder.Console/Models/ParseInput.cs
@@ -0,0 +1,19 @@
+﻿using Oakton;
+
+namespace RssFeeder.Console.Models
+{
+    public class ParseInput
+    {
+        [Description("The URL to parse")]
+        public string Url { get; set; }
+
+        [Description("The article parser to use")]
+        public string Parser { get; set; } = "adaptive-parser";
+
+        [Description("The selector for the body of the article")]
+        public string BodySelector { get; set; } = "article";
+
+        [Description("The selector for the article text paragraphs contained within the body selector")]
+        public string ParagraphSelector { get; set; } = "p";
+    }
+}
