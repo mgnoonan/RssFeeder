5454b6c12fc3950c9e22270e105dd211e35f927b
Matthew Noonan <mnoonan@msn.com>
2022-03-05 22:28:18 -0500

    updated feed builder to grab Noah headlines

--- RssFeeder.Console/FeedBuilders/NoahReportFeedBuilder.cs
+++ RssFeeder.Console/FeedBuilders/NoahReportFeedBuilder.cs
@@ -27,20 +27,20 @@ namespace RssFeeder.Console.FeedBuilders
         public List<RssFeedItem> GenerateRssFeedItemList(string html, List<string> filters, string feedUrl)
         {
             var list = new List<RssFeedItem>();
-            var doc = new HtmlDocument();
-            doc.Load(new StringReader(html));
-
-            // Above the fold headline(s)
-            // //div[@class=\"drudgery-top-links\"]/div/a
-            var nodes = doc.DocumentNode.SelectNodes("//div.widget-area-top-1 > div.sl-links-main > ul > li > a");
             int count;
-            if (nodes != null)
-            {
-                count = 1;
-                foreach (HtmlNode node in nodes)
-                {
-                    string title = WebUtility.HtmlDecode(node.InnerText.Trim());
 
+            // Load and parse the html from the source file
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(html);
+
+            // Above the Fold section
+            var container = document.QuerySelector("div.widget-area-top-1 > div.sl-links-main");
+            if (container != null)
+            {
+                var nodes = container.QuerySelectorAll("a");
+                count = 1;
+                foreach (var node in nodes)
+                {
                     var item = CreateNodeLinks(filters, node, "above the fold", count++, feedUrl);
                     if (item != null)
                     {
@@ -52,14 +52,13 @@ namespace RssFeeder.Console.FeedBuilders
 
             // Featured headline(s)
             // #link-70828 > a
-            nodes = doc.DocumentNode.SelectNodes("//div.widget-area-top-2 > div.sl-links-main > ul > li > a ");
-            if (nodes != null)
+            container = document.QuerySelector("div.widget-area-top-2 > div.sl-links-main");
+            if (container != null)
             {
+                var nodes = container.QuerySelectorAll("a");
                 count = 1;
-                foreach (HtmlNode node in nodes)
+                foreach (var node in nodes)
                 {
-                    string title = WebUtility.HtmlDecode(node.InnerText.Trim());
-
                     var item = CreateNodeLinks(filters, node, "main headlines", count++, feedUrl);
                     if (item != null)
                     {
