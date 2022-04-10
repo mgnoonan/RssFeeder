7b244577ad0000b2fc6e7c47b7542d993528f465
Matthew Noonan <mnoonan@msn.com>
2022-04-10 12:55:04 -0400

    added route template structure to the site article definition model

---
+++ RssFeeder.Models/RouteTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace RssFeeder.Models;
+
+public record RouteTemplate
+{
+    public string Name { get; set; }
+    public string Parser { get; set; }
+    public string Path { get; set; }
+    public string ArticleSelector { get; set; }
+    public string ParagraphSelector { get; set; }
+}
--- RssFeeder.Models/SiteArticleDefinition.cs
+++ RssFeeder.Models/SiteArticleDefinition.cs
@@ -4,11 +4,8 @@ public record SiteArticleDefinition
 {
     public string Id { get; set; }
     public string SiteName { get; set; }
-    public string BulletSelector { get; set; }
     public string ArticleSelector { get; set; }
     public string ParagraphSelector { get; set; }
     public string Parser { get; set; }
-    public string TestArticleFilename { get; set; }
-    public string TestArticleUrl { get; set; }
-    public string TestFeedFilename { get; set; }
+    public RouteTemplate[] Routes { get; set; }
 }
