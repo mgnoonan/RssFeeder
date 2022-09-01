f2c1d67d52056d794078b3074c125f4873f93880
mgnoonan <noonanconsulting@outlook.com>
2022-09-01 07:34:52 -0400

    add ascii(4) to the invalid char list

--- RssFeeder.Mvc/Handlers/GetFeedHandler.cs
+++ RssFeeder.Mvc/Handlers/GetFeedHandler.cs
@@ -77,8 +77,8 @@ namespace RssFeeder.Mvc.Handlers
                         var si = new SyndicationItem()
                         {
                             Id = item.Id,
-                            Title = item.Title.Replace("\u0008", "").Replace("\u0003", "").Replace("\u0010", "").Replace("\u0012", "").Replace("\u0002", "").Replace("\u001f", ""),
-                            Description = item.ArticleText.Replace("\u0008", "").Replace("\u0003", "").Replace("\u0010", "").Replace("\u0012", "").Replace("\u0002", "").Replace("\u001f", ""),
+                            Title = item.Title.Replace("\u0008", "").Replace("\u0003", "").Replace("\u0010", "").Replace("\u0012", "").Replace("\u0002", "").Replace("\u001f", "").Replace("\u0004", ""),
+                            Description = item.ArticleText.Replace("\u0008", "").Replace("\u0003", "").Replace("\u0010", "").Replace("\u0012", "").Replace("\u0002", "").Replace("\u001f", "").Replace("\u0004", ""),
                             Published = item.DateAdded,
                             LastUpdated = item.DateAdded
                         };
