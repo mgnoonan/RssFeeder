da772a30c7849551705f19c018315e4ab117bcbe
Matthew Noonan <mnoonan@msn.com>
2022-11-03 06:06:34 -0400

    removed some unluanch variations

--- RssFeeder.Console/FeedBuilders/RevolverNewsFeedBuilder.cs
+++ RssFeeder.Console/FeedBuilders/RevolverNewsFeedBuilder.cs
@@ -3,32 +3,17 @@
 internal class RevolverNewsFeedBuilder : BaseFeedBuilder, IRssFeedBuilder
 {
     private readonly IUnlaunchClient _client;
-    private int _articleMaxCount;
+    private readonly int _articleMaxCount;
 
     public RevolverNewsFeedBuilder(ILogger logger, IWebUtils webUtilities, IUtils utilities, IUnlaunchClient client) :
         base(logger, webUtilities, utilities)
     {
         _client = client;
+        _articleMaxCount = 1000;
     }
 
     public List<RssFeedItem> GenerateRssFeedItemList(RssFeed feed, string html)
     {
-        // Find out which feature flag variation we are using to crawl articles
-        string key = "article-count-limit";
-        string identity = feed.CollectionName;
-        string variation = _client.GetVariation(key, identity);
-        _log.Information("Unlaunch {key} returned variation {variation} for identity {identity}", key, variation, identity);
-
-        _articleMaxCount = variation switch
-        {
-            "high" => 100,
-            "medium" => 50,
-            "low" => 25,
-            "unlimited" => 1000,
-            _ => throw new ArgumentException("Unexpected variation")
-        };
-        _log.Information("Processing a maximum of {articleMaxCount} articles", _articleMaxCount);
-
         return GenerateRssFeedItemList(feed.CollectionName, feed.Url, feed.Filters, html);
     }
 
