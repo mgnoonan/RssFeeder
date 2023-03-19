c2927b17fb01270c98972fe018c8c021ce590d25
mgnoonan <noonanconsulting@outlook.com>
2023-03-19 11:48:27 -0400

    improved duplicate image detection

--- RssFeeder.Console/TagParsers/TagParserBase.cs
+++ RssFeeder.Console/TagParsers/TagParserBase.cs
@@ -15,6 +15,7 @@ public partial class TagParserBase
     protected RssFeedItem _item;
 
     private const string _sizePattern = @"\d{3,4}x\d{3,4}";
+    private const string _sizePattern2 = @"/ALTERNATES/s\d{3,4}";
 
     public TagParserBase(ILogger log, IUnlaunchClient client, IWebUtils webUtils)
     {
@@ -244,19 +245,38 @@ public partial class TagParserBase
 
     private bool ImageSourcesAreEqual(string value1, string value2)
 	{        
-		if (value1.Length == value2.Length)
-		{
-		    bool containsSize = Regex.IsMatch(value1, _sizePattern);
-			if (containsSize)
-			{
-				value1 = Regex.Replace(value1, _sizePattern, "");
-				value2 = Regex.Replace(value2, _sizePattern, "");
-			}
+        if (Regex.IsMatch(value1, _sizePattern))
+        {
+            value1 = Regex.Replace(value1, _sizePattern, "");
+            value2 = Regex.Replace(value2, _sizePattern, "");
+        }
+        if (Regex.IsMatch(value1, _sizePattern2))
+        {
+            value1 = Regex.Replace(value1, _sizePattern2, "");
+            value2 = Regex.Replace(value2, _sizePattern2, "");
+        }
 
-			return value1 == value2;
-		}
+        // Decode any URL encoding sometimes used for CDN references
+		value1 = value1.Contains("%3A") ? System.Web.HttpUtility.UrlDecode(value1) : value1;
+		value2 = value2.Contains("%3A") ? System.Web.HttpUtility.UrlDecode(value2) : value2;
+
+		// Strip off the query string
+        value1 = value1.Contains("?") ? value1.Substring(0, value1.IndexOf('?')) : value1;
+		value2 = value2.Contains("?") ? value2.Substring(0, value2.IndexOf('?')) : value2;
+
+		// WSJ has a special route for social media thumbnails
+ 		value1 = value1.EndsWith("/social") ? value1.Substring(0, value1.IndexOf("/social")) : value1;
+		value2 = value2.EndsWith("/social") ? value2.Substring(0, value2.IndexOf("/social")) : value2;
+
+		// Yahoo CDN route handling
+        value1 = value1.Contains("--/") ? value1.Substring(value1.LastIndexOf("--/") + 3) : value1;
+		value2 = value2.Contains("--/") ? value2.Substring(value2.LastIndexOf("--/") + 3) : value2;
 		
-		return false;
+        // Substack CDN route handling
+		value1 = value1.Contains("/https") ? value1.Substring(value1.LastIndexOf("/https") + 1) : value1;
+		value2 = value2.Contains("/https") ? value2.Substring(value2.LastIndexOf("/https") + 1) : value2;
+
+		return value1 == value2;
 	}
 
     private void RemoveElementPadding(IHtmlDocument document)
