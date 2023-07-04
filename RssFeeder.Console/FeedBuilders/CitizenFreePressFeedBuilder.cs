bf281ddfdf257029ad55670f76be875e8d826d03
Matthew Noonan <mnoonan@msn.com>
2023-07-04 15:20:58 -0400

    refactor cfp

--- RssFeeder.Console/FeedBuilders/CitizenFreePressFeedBuilder.cs
+++ RssFeeder.Console/FeedBuilders/CitizenFreePressFeedBuilder.cs
@@ -36,95 +36,21 @@ class CitizenFreePressFeedBuilder : BaseFeedBuilder, IRssFeedBuilder
     public List<RssFeedItem> GenerateRssFeedItemList(string html)
     {
         var list = new List<RssFeedItem>();
-        int count;
-
-        // Above the Fold section
-        var container = _document.QuerySelector("ul.wpd-top-links");
-        var nodes = container.QuerySelectorAll("a");
-        if (nodes != null)
-        {
-            count = 1;
-            foreach (var node in nodes)
-            {
-                var item = CreateNodeLinks(_feedFilters, node, "above the fold", count++, _feedUrl, true);
-                if (item != null)
-                {
-                    _log.Write(_logLevel, "FOUND: {urlHash}|{linkLocation}|{title}|{url}", item.FeedAttributes.UrlHash, item.FeedAttributes.LinkLocation, item.FeedAttributes.Title, item.FeedAttributes.Url);
-                    list.Add(item);
-                }
-            }
-        }
 
         // Main Headlines section
-        container = _document.QuerySelector("#featured");
-        if (container != null)
-        {
-            nodes = container.QuerySelectorAll("a");
-            if (nodes != null)
-            {
-                count = 1;
-                foreach (var node in nodes)
-                {
-                    var item = CreateNodeLinks(_feedFilters, node, "main headlines", count++, _feedUrl, true);
-                    if (item != null && !item.FeedAttributes.Url.Contains("#the-comments") && !item.FeedAttributes.Url.Contains("#comment-"))
-                    {
-                        _log.Write(_logLevel, "FOUND: {urlHash}|{linkLocation}|{title}|{url}", item.FeedAttributes.UrlHash, item.FeedAttributes.LinkLocation, item.FeedAttributes.Title, item.FeedAttributes.Url);
-                        list.Add(item);
-                    }
-                }
-            }
-        }
+        GetNodeLinks("headlines", "#featured", "a.headline-link", list, false);
+
+        // Above the Fold section
+        GetNodeLinks("above the fold", "ul.wpd-top-links", "a.headline-link", list, false);
 
         // Column 1
-        container = _document.QuerySelector("#column-1");
-        nodes = container.QuerySelectorAll("a");
-        if (nodes != null)
-        {
-            count = 1;
-            foreach (var node in nodes)
-            {
-                var item = CreateNodeLinks(_feedFilters, node, "column 1", count++, _feedUrl, false);
-                if (item != null && !item.FeedAttributes.Url.Contains("/column-1/") && !item.FeedAttributes.Url.Contains("#the-comments") && !item.FeedAttributes.Url.Contains("#comment-"))
-                {
-                    _log.Write(_logLevel, "FOUND: {urlHash}|{linkLocation}|{title}|{url}", item.FeedAttributes.UrlHash, item.FeedAttributes.LinkLocation, item.FeedAttributes.Title, item.FeedAttributes.Url);
-                    list.Add(item);
-                }
-            }
-        }
+        GetNodeLinks("column 1", "#column-1", "a.headline-link", list, false);
 
         // Column 2
-        container = _document.QuerySelector("#column-2");
-        nodes = container.QuerySelectorAll("a");
-        if (nodes != null)
-        {
-            count = 1;
-            foreach (var node in nodes)
-            {
-                var item = CreateNodeLinks(_feedFilters, node, "column 2", count++, _feedUrl, false);
-                if (item != null && !item.FeedAttributes.Url.Contains("/column-2/") && !item.FeedAttributes.Url.Contains("#the-comments") && !item.FeedAttributes.Url.Contains("#comment-"))
-                {
-                    _log.Write(_logLevel, "FOUND: {urlHash}|{linkLocation}|{title}|{url}", item.FeedAttributes.UrlHash, item.FeedAttributes.LinkLocation, item.FeedAttributes.Title, item.FeedAttributes.Url);
-                    list.Add(item);
-                }
-            }
-        }
+        GetNodeLinks("column 2", "#column-2", "a.headline-link", list, false);
 
         // Column 3
-        container = _document.QuerySelector("#column-3");
-        nodes = container.QuerySelectorAll("a");
-        if (nodes != null)
-        {
-            count = 1;
-            foreach (var node in nodes)
-            {
-                var item = CreateNodeLinks(_feedFilters, node, "column 3", count++, _feedUrl, false);
-                if (item != null && !item.FeedAttributes.Url.Contains("/column-3/") && !item.FeedAttributes.Url.Contains("#the-comments") && !item.FeedAttributes.Url.Contains("#comment-"))
-                {
-                    _log.Write(_logLevel, "FOUND: {urlHash}|{linkLocation}|{title}|{url}", item.FeedAttributes.UrlHash, item.FeedAttributes.LinkLocation, item.FeedAttributes.Title, item.FeedAttributes.Url);
-                    list.Add(item);
-                }
-            }
-        }
+        GetNodeLinks("column 3", "#column-3", "a.headline-link", list, false);
 
         return list;
     }
