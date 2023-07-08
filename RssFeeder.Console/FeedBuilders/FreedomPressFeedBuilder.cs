1bdc3b6e65a6e9e8edc07814e7afacf84ec2d7db
Matthew Noonan <mnoonan@msn.com>
2023-07-07 21:35:12 -0400

    refactor freedom press

--- RssFeeder.Console/FeedBuilders/FreedomPressFeedBuilder.cs
+++ RssFeeder.Console/FeedBuilders/FreedomPressFeedBuilder.cs
@@ -37,6 +37,22 @@ internal class FreedomPressFeedBuilder : BaseFeedBuilder, IRssFeedBuilder
     {
         var list = new List<RssFeedItem>();
 
+        // Main headlines section
+        // #fg-widget-65cf26586c253334929d032bf > div.uw-sc-mask > div.uw-sc-cardcont > div.uw-card2.uw-headline > a
+        GetNodeLinks("headlines", "#fg-widget-65cf26586c253334929d032bf > div.uw-sc-mask > div.uw-sc-cardcont", "div > a", list, false);
+
+        // Column 1 section
+        // #fg-widget-fd07a65186f463b9a3ff917c1 > div.uw-sc-mask > div.uw-sc-cardcont > div.uw-card2.uw-headline > a
+        GetNodeLinks("column 1", "#fg-widget-fd07a65186f463b9a3ff917c1 > div.uw-sc-mask > div.uw-sc-cardcont", "div > a", list, false);
+
+        // Column 2 section
+        // #fg-widget-f9e047dd6b8da3cdc21edad8d > div.uw-sc-mask > div.uw-sc-cardcont > div.uw-card2.uw-headline > a
+        GetNodeLinks("column 2", "#fg-widget-f9e047dd6b8da3cdc21edad8d > div.uw-sc-mask > div.uw-sc-cardcont", "div > a", list, false);
+
+        // Column 3 section
+        // #fg-widget-a52b0845e1da669a0aa917645 > div.uw-sc-mask > div.uw-sc-cardcont > div.uw-card2.uw-headline > a
+        GetNodeLinks("column 3", "#fg-widget-a52b0845e1da669a0aa917645 > div.uw-sc-mask > div.uw-sc-cardcont", "div > a", list, false);
+
         return list;
     }
 }
