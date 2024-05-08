cd65d016c270aa3d94f891ccca3233acb71795e6
Matthew Noonan <mnoonan@msn.com>
2024-05-08 17:52:56 -0400

    Refactored code and restructured metadata application logic
    Refactored the code for improved readability and maintainability by extracting several blocks of code into separate methods. Removed comments related to `UrlHash`, indicating possible changes to its functionality. Introduced new methods for various tasks such as checking if a file is a graphic file, retrieving video URL, determining video type, checking video format support, retrieving parsed result, and applying basic and video templates. Restructured the logic for applying metadata values and templates based on the type of file and video format. Added logging for better visibility into the process of applying metadata values and templates.
    
    http://sonarqube:9000/coding_rules?open=csharpsquid%3AS3776&rule_key=csharpsquid%3AS3776

--- RssFeeder.Console/Exporters/ArticleExporter.cs
+++ RssFeeder.Console/Exporters/ArticleExporter.cs
@@ -11,11 +11,6 @@ public class ArticleExporter : BaseArticleExporter, IArticleExporter
 
     public ExportFeedItem FormatItem(RssFeedItem item, RssFeed feed)
     {
-        // The UrlHash is a hash of the feed source, not the ultimate target URL. This is to avoid
-        // over-crawling with link shortening services such as bit.ly and t.co. Once we detect a hash
-        // has been crawled from the source, there is no need to crawl again. It means the hash does
-        // not truly reflect the target URL, but that's ok as there are duplicate crawls across the 
-        // different feeds anyway.
         var exportFeedItem = new ExportFeedItem
         {
             Id = Guid.NewGuid().ToString(),
@@ -32,25 +27,72 @@ public class ArticleExporter : BaseArticleExporter, IArticleExporter
         string hostName = uri.GetComponents(UriComponents.Host, UriFormat.Unescaped).ToLower();
 
         var fileName = item.FeedAttributes.FileName ?? "";
-        if (fileName.EndsWith(".png") ||
-            fileName.EndsWith(".jpg") ||
-            fileName.EndsWith(".gif") ||
-            fileName.EndsWith(".pdf") ||
-            fileName.EndsWith(".mp3") ||
-            fileName.EndsWith(".webp"))
+        if (IsGraphicFile(fileName))
         {
             SetGraphicMetaData(item, exportFeedItem);
             exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.GraphicTemplate);
             return exportFeedItem;
         }
 
-        string videoUrl = item.OpenGraphAttributes.GetValueOrDefault("og:video:secure_url") ??
+        string videoUrl = GetVideoUrl(item);
+        string videoType = GetVideoType(item, videoUrl);
+
+        if (HasSupportedVideoFormat(item, videoUrl, videoType))
+        {
+            _log.Debug("Applying video metadata values for '{hostname}'", hostName);
+            SetVideoMetaData(exportFeedItem, item, hostName);
+            if (exportFeedItem.VideoHeight > 0)
+            {
+                exportFeedItem.ArticleText = GetVideoTemplate(exportFeedItem, feed, videoType);
+            }
+            else
+            {
+                exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.ExtendedTemplate);
+            }
+        }
+        else
+        {
+            bool useTitle = item.HostName == "twitter.com";
+            string result = GetParsedResult(item, useTitle);
+            string imageUrl = item.OpenGraphAttributes.GetValueOrDefault("og:image") ?? null;
+            if (string.IsNullOrEmpty(result))
+            {
+                _log.Information("No parsed result, applying basic metadata values for '{hostname}'", hostName);
+                SetBasicArticleMetaData(exportFeedItem, item, hostName);
+                exportFeedItem.ArticleText = GetBasicTemplate(exportFeedItem, feed, imageUrl);
+            }
+            else
+            {
+                _log.Information("Applying extended metadata values for '{hostname}'", hostName);
+                SetExtendedArticleMetaData(exportFeedItem, item, hostName);
+                exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.ExtendedTemplate);
+            }
+        }
+
+        return exportFeedItem;
+    }
+
+    private bool IsGraphicFile(string fileName)
+    {
+        return fileName.EndsWith(".png") ||
+            fileName.EndsWith(".jpg") ||
+            fileName.EndsWith(".gif") ||
+            fileName.EndsWith(".pdf") ||
+            fileName.EndsWith(".mp3") ||
+            fileName.EndsWith(".webp");
+    }
+
+    private string GetVideoUrl(RssFeedItem item)
+    {
+        return item.OpenGraphAttributes.GetValueOrDefault("og:video:secure_url") ??
             item.OpenGraphAttributes.GetValueOrDefault("og:video:url") ??
             item.OpenGraphAttributes.GetValueOrDefault("og:video") ??
             item.OpenGraphAttributes.GetValueOrDefault("og:x:video") ??
             "";
+    }
 
-        // Some sites do not provide OpenGraph video tags so watch for those specifically
+    private string GetVideoType(RssFeedItem item, string videoUrl)
+    {
         string videoType;
         if (item.OpenGraphAttributes.TryGetValue("og:video:type", out string value))
         {
@@ -73,51 +115,34 @@ public class ArticleExporter : BaseArticleExporter, IArticleExporter
             videoType = "";
         }
 
-        bool hasSupportedVideoFormat = (videoUrl.Length > 0 || item.SiteName == "rumble" || item.SiteName == "bitchute") &&
-            (videoType == "text/html" || videoType == "video/mp4" || videoType == "application/x-mpegURL");
+        return videoType;
+    }
 
-        if (hasSupportedVideoFormat)
+    private bool HasSupportedVideoFormat(RssFeedItem item, string videoUrl, string videoType)
+    {
+        return (videoUrl.Length > 0 || item.SiteName == "rumble" || item.SiteName == "bitchute") &&
+            (videoType == "text/html" || videoType == "video/mp4" || videoType == "application/x-mpegURL");
+    }
+
+    private string GetParsedResult(RssFeedItem item, bool useTitle)
+    {
+        return useTitle ? item.OpenGraphAttributes.GetValueOrDefault("og:title") : item.HtmlAttributes.GetValueOrDefault("ParserResult") ?? "";
+    }
+
+    private string GetBasicTemplate(ExportFeedItem exportFeedItem, RssFeed feed, string imageUrl)
+    {
+        return ApplyTemplateToDescription(exportFeedItem, feed, string.IsNullOrEmpty(imageUrl) ? ExportTemplates.BasicTemplate : ExportTemplates.BasicPlusTemplate);
+    }
+
+    private string GetVideoTemplate(ExportFeedItem exportFeedItem, RssFeed feed, string videoType)
+    {
+        if (videoType == "video/mp4" || videoType == "application/x-mpegURL")
         {
-            _log.Debug("Applying video metadata values for '{hostname}'", hostName);
-            SetVideoMetaData(exportFeedItem, item, hostName);
-            if (exportFeedItem.VideoHeight > 0)
-            {
-                if (videoType == "video/mp4" || videoType == "application/x-mpegURL")
-                {
-                    exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.Mp4VideoTemplate);
-                }
-                else
-                {
-                    exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.HtmlVideoTemplate);
-                }
-            }
-            else
-            {
-                exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.ExtendedTemplate);
-            }
+            return ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.Mp4VideoTemplate);
         }
         else
         {
-            bool useTitle = item.HostName == "twitter.com";
-            string result = useTitle ? item.OpenGraphAttributes.GetValueOrDefault("og:title") : item.HtmlAttributes.GetValueOrDefault("ParserResult") ?? "";
-            string imageUrl = item.OpenGraphAttributes.GetValueOrDefault("og:image") ?? null;
-            if (string.IsNullOrEmpty(result))
-            {
-                _log.Information("No parsed result, applying basic metadata values for '{hostname}'", hostName);
-
-                // Article failed to download, display minimal basic meta data
-                SetBasicArticleMetaData(exportFeedItem, item, hostName);
-                exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, string.IsNullOrEmpty(imageUrl) ? ExportTemplates.BasicTemplate : ExportTemplates.BasicPlusTemplate);
-            }
-            else
-            {
-                _log.Information("Applying extended metadata values for '{hostname}'", hostName);
-
-                SetExtendedArticleMetaData(exportFeedItem, item, hostName);
-                exportFeedItem.ArticleText = ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.ExtendedTemplate);
-            }
+            return ApplyTemplateToDescription(exportFeedItem, feed, ExportTemplates.HtmlVideoTemplate);
         }
-
-        return exportFeedItem;
     }
 }
